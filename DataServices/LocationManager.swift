94223e85a6f7867c779414fc104c7c37c65c6252
UOA1992 <UOA1992@github.com>
2018-09-09 17:13:27 -0400

    009 09 18 5:13 PM

---
+++ DataServices/LocationManager.swift
@@ -0,0 +1,9 @@
+//
+//  LocationManager.swift
+//  HeavyRain6
+//
+//  Created by Edward L Richardson on 9/9/18.
+//  Copyright © 2018 Edward L Richardson. All rights reserved.
+//
+
+import Foundation
---
+++ DataServices/WeatherAPIManager.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherAPIManager.swift
+//  HeavyRain6
+//
+//  Created by Edward L Richardson on 9/9/18.
+//  Copyright © 2018 Edward L Richardson. All rights reserved.
+//
+
+import Foundation
