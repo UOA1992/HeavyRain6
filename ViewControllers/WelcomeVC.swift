636136e8b4c860b7c92d54ce02bb2584a8f47e39
UOA1992 <UOA1992@github.com>
2018-09-09 10:23:51 -0400

    Initial Commit

---
+++ ViewControllers/WelcomeVC.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomeVC.swift
+//  HeavyRain6
+//
+//  Created by Edward L Richardson on 9/9/18.
+//  Copyright © 2018 Edward L Richardson. All rights reserved.
+//
+
+import UIKit
+
+
+
+
